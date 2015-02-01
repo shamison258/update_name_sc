2de0c7fd4fca51548832d013545a44ae0b4e1d94
shamison <unknown0incident@gmail.com>
2015-02-02 06:02:27 +0900

    add stream function

--- src/main/scala/com/shamison/Main.scala
+++ src/main/scala/com/shamison/Main.scala
@@ -1,10 +1,21 @@
 package com.shamison
 
+import twitter4j._
+
 /**
  * Created by shamison on 15/02/01.
  */
 object Main {
 	def main(args: Array[String]) = {
 		println("Hello, World")
+		val listener: StatusListener = new StatusAdapter {
+			override def onStatus(status: Status): Unit = {
+				println(status.getText)
+			}
+		}
+		val twitter: TwitterStream = new TwitterStreamFactory().getInstance()
+		twitter.addListener(listener)
+		twitter.user()
 	}
 }
+

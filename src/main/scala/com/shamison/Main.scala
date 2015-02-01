b312b5e23e53f2c67aeffba44f9dc1dfe4783aa3
shamison <unknown0incident@gmail.com>
2015-02-02 06:27:09 +0900

    modify: Use UserStreamAdapter
    this is useful than before

--- src/main/scala/com/shamison/Main.scala
+++ src/main/scala/com/shamison/Main.scala
@@ -8,14 +8,25 @@ import twitter4j._
 object Main {
 	def main(args: Array[String]) = {
 		println("Hello, World")
-		val listener: StatusListener = new StatusAdapter {
-			override def onStatus(status: Status): Unit = {
-				println(status.getText)
-			}
-		}
+		val listener: StatusListener = Listener
 		val twitter: TwitterStream = new TwitterStreamFactory().getInstance()
 		twitter.addListener(listener)
 		twitter.user()
 	}
+}
+
+object Listener extends UserStreamAdapter {
+	override def onStatus(status: Status): Unit = {
+		print(status.getUser.getName + ": ")
+		println(status.getText)
+	}
+
+	override def onFavorite(source: User, target: User, favoritedStatus: Status){
+		print(source.getName + " fav to ")
+		println(target.getName)
+	}
+
+
+
 }
 

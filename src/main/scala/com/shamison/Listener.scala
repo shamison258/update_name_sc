f8ed51f6792c8c1e92d535cf559cf600d22e1a48
shamison <unknown0incident@gmail.com>
2015-02-16 18:01:16 +0900

    coplete project

--- src/main/scala/com/shamison/Listener.scala
+++ src/main/scala/com/shamison/Listener.scala
@@ -6,19 +6,24 @@ import twitter4j._
 /**
  * Created by shamison on 15/02/14.
  */
-class Listener extends UserStreamAdapter {
-	override def onStatus(status: Status): Unit = {
-		println(status.getUser.getName
-			+ "(@" + status.getUser.getScreenName + ")"+ ": ")
-		println(status.getText)
-		println()
+class Listener(userId:String) extends UserStreamAdapter {
+
+  override def onStatus(status: Status): Unit = {
+//		println(status.getUser.getName
+//			+ "(@" + status.getUser.getScreenName + ")"+ ": ")
+//		println(status.getText)
+//		println()
+    val tlText = status.getText
+    if (tlText.startsWith("@"+userId +" update_name ") ||
+      tlText.endsWith("(@"+Main.userId+")"))
+      new updateName(status).update()
 	}
 
-	override def onFavorite(source: User, target: User, favoritedStatus: Status){
-		println("[FAV]:"+ source.getName
-			+ "(" + source.getScreenName + ")"+ " to ")
-		println(favoritedStatus.getText)
-		println()
-	}
+//	override def onFavorite(source: User, target: User, favoritedStatus: Status){
+//		println("[FAV]:"+ source.getName
+//			+ "(" + source.getScreenName + ")"+ " to ")
+//		println(favoritedStatus.getText)
+//		println()
+//	}
 
 }
--- src/main/scala/com/shamison/Main.scala
+++ src/main/scala/com/shamison/Main.scala
@@ -6,13 +6,16 @@ import twitter4j._
  * Created by shamison on 15/02/01.
  */
 object Main {
-	def main(args: Array[String]) = {
-		println("Hello, World")
-		val listener: StatusListener = new Listener
-		val twitter: TwitterStream = new TwitterStreamFactory().getInstance()
+  val userId = "_sham258"
+  val tw :Tweet = new Tweet()
+  val listener: StatusListener = new Listener(userId)
+  val twitter: TwitterStream = new TwitterStreamFactory().getInstance()
+
+  def main(args: Array[String]) = {
 		twitter.addListener(listener)
 		twitter.user()
-	}
+    tw.tweet("update_name 起動しました")
+  }
 }
 
 

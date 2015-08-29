ba0f533015319a77e1614b78508d3f011a74bf3a
shamison <unknown0incident@gmail.com>
2015-08-29 15:10:43 +0900

    modify match
    型でパターンマッチするようにした。
    文字を正規表現で取り出すようにした。
    それに伴って各関数の引数を削減。
    twitterとtweet関数をMasterに移動。

--- src/main/scala/com/shamison/Main.scala
+++ src/main/scala/com/shamison/Main.scala
@@ -9,35 +9,43 @@ import twitter4j._
  * Created by shamison on 15/02/01.
  */
 object Main {
-	val twitter = new TwitterFactory().getInstance
+
   val stream = new TwitterStreamFactory().getInstance
   stream.addListener(Listener())
 
   val system = ActorSystem("master")
   val actor = system.actorOf(Props[Master])
 
-  case class Data(t: Twitter, s: Status, value: String)
+  case class NameData(s: Status, v: String)
+  case class IconData(s: Status)
+  case class DefData(s: Status)
+  case class SonData(s: Status, v: String)
 
   case class Listener() extends UserStreamAdapter {
-    val updateName = """^@_sham258 update_name .*""".r
-    val updateNamePrefix = """.*\(@_sham258\s?\)$""".r
+    val updateName = """^@_sham258 update_name (.*).*""".r
+    val updateNamePrefix = """(.*).*\(@_sham258\s+?\)$""".r
     val updateIcon = """^@_sham258 update_icon .*""".r
     val updateDefault = """^@_sham258 update_default.*""".r
-    val updateSon = """.*そん$""".r
+    val updateSon = """^(.*そん)$""".r
 
     override def onStatus(status: Status) = status.getText match {
-      case updateName() | updateNamePrefix() =>
-        actor ! Data(twitter, status, "name")
+      case updateName(s) =>
+        actor ! NameData(status, s)
+      case updateNamePrefix(s) =>
+        actor ! NameData(status, s)
+
       case updateIcon() =>
-        actor ! Data(twitter, status, "icon")
+        actor ! IconData(status)
+
       case updateDefault() if status.getUser.getScreenName == "_sham258"=>
-        actor ! Data(twitter, status, "def")
-      case updateSon() =>
-        actor ! Data(twitter, status, "son")
+        actor ! DefData(status)
+
+      case updateSon(s) =>
+        actor ! SonData(status, s)
     }
 	}
-  
-	def tweet(s: String) = twitter.updateStatus(s)
+
+
 
 	def main(args: Array[String]) = stream.user()
 
--- src/main/scala/com/shamison/Master.scala
+++ src/main/scala/com/shamison/Master.scala
@@ -5,7 +5,7 @@ import java.net.{URL, HttpURLConnection}
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, Props, ActorSystem}
 import akka.routing.RoundRobinRouter
-import twitter4j.{MediaEntity, Status, Twitter}
+import twitter4j.{TwitterFactory, MediaEntity, Status, Twitter}
 
 /**
  * Created by shamison on 15/08/20.
@@ -16,95 +16,77 @@ class Master extends Actor {
   val defRouter = context.actorOf(Props(classOf[DefActor], this).withRouter(RoundRobinRouter(2)))
   val sonRouter = context.actorOf(Props(classOf[SonActor], this).withRouter(RoundRobinRouter(2)))
 
+  val twitter = new TwitterFactory().getInstance
+
+  def tweet(s: String) = twitter.updateStatus(s)
+  def tweet(s: String, status: Status) = twitter
+    .updateStatus(s + " https://twitter.com/" +
+    status.getUser.getScreenName + "/status/" + status.getId)
+
   override def receive = {
-    case data: Main.Data => data.value match {
-      case "name" => nameRouter ! data
-      case "icon" => iconRouter ! data
-      case "def" => defRouter ! data
-      case "son" => sonRouter ! data
-    }
+    case data: Main.NameData => nameRouter ! data
+    case data: Main.IconData => iconRouter ! data
+    case data: Main.DefData => defRouter ! data
+    case data: Main.SonData => sonRouter ! data
     case x => println(x)
   }
 
   class SonActor extends Actor {
     override def receive = {
-      case data: Main.Data => son(data.s, data.t)
+      case data: Main.SonData => son(data.v)
     }
 
-    def son(status: Status, twitter: Twitter) = status.getText match {
-      case name: String if name.length <= 20 =>
-        twitter.updateProfile(name, null, null, null)
-    }
+    def son(name: String) = if(name.length <= 20)
+      twitter.updateProfile(name, null, null, null)
   }
-
 
   class DefActor extends Actor{
     override def receive = {
-      case data: Main.Data => default(data.s, data.t)
+      case data: Main.DefData => default(data.s)
     }
-
-    def default(status: Status, twitter: Twitter) = {
+    def default(status: Status) = {
       val name = "shamison"
-      val twitUrl = "https://twitter.com/" +
-        status.getUser.getScreenName + "/status/" + status.getId
-
       twitter.updateProfile(name, null, null, null)
+
       val in = Main.getClass.getClassLoader.getResourceAsStream("icon.jpg")
       twitter.updateProfileImage(in)
-      Main.tweet("デフォルト設定に戻しました。 " + twitUrl)
 
+      tweet("デフォルト設定に戻しました。", status)
     }
   }
 
   class NameActor extends Actor {
     override def receive = {
-      case data: Main.Data => name(data.s, data.t)
+      case data: Main.NameData => name(data.s, data.v)
     }
 
-    def name(status: Status, twitter: Twitter): Boolean = {
-      val twitUrl = "https://twitter.com/" + status.getUser.getScreenName +
-        "/status/" + status.getId
-      val name = status.getText
-        .replaceAll("""\(@_sham258(\s+)?\)""", "")
-        .replace("@_sham258 update_name ", "")
-        .trim
-
-      println(name)
-
-      name match {
-        case "" =>
-          false
+    def name(status: Status, name: String): Boolean = name match {
+        case "" => false
         case n if n.length <= 20 =>
           twitter.updateProfile(name, null, null, null)
-          Main.tweet("「%s」に改名しました。 %s"
-            .format(n, twitUrl).replaceAll("@", "-"))
+          tweet("「%s」に改名しました。"
+            .replaceAll("@", "-"), status)
           true
-      }
     }
   }
 
   class IconActor extends Actor {
     override def receive = {
-      case data: Main.Data => icon(data.s, data.t)
+      case data: Main.IconData => icon(data.s)
     }
 
-    def icon(status: Status, twitter: Twitter): Boolean = {
-      val twitUrl = "https://twitter.com/" + status.getUser.getScreenName +
-        "/status/" + status.getId
-      status.getMediaEntities.apply(0).getMediaURLHttps match {
-        case "" | null =>
-          Main.tweet("アイコンを変えられませんでした。 %s".format(twitUrl))
-          false
-        case s =>
-          val connection = new URL(s)
-            .openConnection
-            .asInstanceOf[HttpURLConnection]
-          connection.setRequestMethod("GET")
-          twitter.updateProfileImage(connection.getInputStream)
-          Main.tweet("アイコンを変更しました。 %s".format(twitUrl))
-          true
-      }
+    def icon(status: Status): Boolean = status.getMediaEntities
+      .apply(0).getMediaURLHttps match {
+      case "" | null =>
+        tweet("アイコンを変えられませんでした。", status)
+        false
+      case s =>
+        val connection = new URL(s).openConnection
+          .asInstanceOf[HttpURLConnection]
+        connection.setRequestMethod("GET")
+        twitter.updateProfileImage(connection.getInputStream)
+        tweet("アイコンを変更しました。", status)
+        true
     }
   }
-
 }
